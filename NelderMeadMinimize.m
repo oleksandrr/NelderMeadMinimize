df79149c93584ab4c4bc6396217cdf0ad60f4bf0
oleksandrr <oleksandr_rasputinov@ymail.com>
2013-04-09 08:37:02 +0100

    version 3

--- NelderMeadMinimize.m
+++ NelderMeadMinimize.m
@@ -3,58 +3,65 @@
 BeginPackage["NelderMeadMinimize`"];
 
 ClearAll[NelderMeadMinimize];
-Attributes[NelderMeadMinimize] = {HoldAll};
+SetAttributes[NelderMeadMinimize, HoldAll];
 Options[NelderMeadMinimize] = {
-   "InitialPoints" -> Automatic, "RandomSeed" -> Sequence[0, Method -> "MKL"],
-   "ConvergenceTolerance" -> $MachineEpsilon, MaxIterations -> Infinity,
-   "HistoryLength" -> Automatic,
-   "ReflectRatio" -> 1, "ExpandRatio" -> 2, "ContractRatio" -> 1/2, "ShrinkRatio" -> 1/2,
-   CompilationTarget :> $CompilationTarget
-  };
+    "InitialPoints" -> Automatic, "RandomSeed" -> Sequence[0, Method -> "MKL"],
+    "ConvergenceTolerance" -> $MachineEpsilon, "HistoryLength" -> Automatic, MaxIterations -> Infinity,
+    "ReflectRatio" -> 1, "ExpandRatio" -> 2, "ContractRatio" -> 1/2, "ShrinkRatio" -> 1/2,
+    "Diagnostics" -> False, CompilationTarget :> $CompilationTarget
+   } // Sort;
 
 ClearAll[NelderMeadMinimize`Dump`CompiledNelderMead];
 Options[NelderMeadMinimize`Dump`CompiledNelderMead] = {
-   "HistoryLength" -> Automatic,
-   "ReflectRatio" -> 1, "ExpandRatio" -> 2, "ContractRatio" -> 1/2, "ShrinkRatio" -> 1/2,
-   CompilationTarget :> $CompilationTarget, "ReturnValues" -> "AugmentedOptimizedParameters"
-  };
+    "HistoryLength" -> Automatic,
+    "ReflectRatio" -> 1, "ExpandRatio" -> 2, "ContractRatio" -> 1/2, "ShrinkRatio" -> 1/2,
+    "ReturnValues" -> "AugmentedOptimizedParameters",
+    "Diagnostics" -> False, CompilationTarget :> $CompilationTarget
+   } // Sort;
 
-ClearAll[NelderMeadMinimize`Dump`Rosenbrock];
+(* This value will be set on return if diagnostics are enabled *)
+ClearAll[NelderMeadMinimize`Dump`OperationCounts];
 
+(* Separable and nonseparable hyperellipsoid functions *)
 ClearAll[NelderMeadMinimize`Dump`Hyperellipsoid];
+ClearAll[NelderMeadMinimize`Dump`RotatedHyperellipsoid];
+
+(* Rosenbrock's function *)
+ClearAll[NelderMeadMinimize`Dump`Rosenbrock];
 
 Begin["`Private`"];
 
 (* Caller for the compiled Nelder-Mead code *)
 NelderMeadMinimize[
-   minimand_, vars : {__Symbol},
+   minimand_, vars : {varseq__Symbol},
    opts : OptionsPattern[NelderMeadMinimize]
   ] :=
-  BlockRandom@Block[vars,
+  BlockRandom@Block[{varseq, dimension = Length[vars]},
     SeedRandom@OptionValue["RandomSeed"];
-    Module[{
-      dimensions = Length[vars], initialPoints,
-      objectiveFunction = Switch[Head[minimand],
-        Function | CompiledFunction, minimand,
-        _, Function[vars, minimand]
+    With[{
+      compiledMinimizer = NelderMeadMinimize`Dump`CompiledNelderMead[
+        Switch[Head[minimand],
+         Function | CompiledFunction, minimand,
+         _, Function[vars, minimand]
+        ], vars,
+        Sort@FilterRules[
+          {opts}~Join~FilterRules[Options[NelderMeadMinimize], Except[{opts}]],
+          Options[NelderMeadMinimize`Dump`CompiledNelderMead]
+         ], "ReturnValues" -> "AugmentedOptimizedParameters"
        ],
-      compiledMinimizer,
-      maxIterations = If[# === Infinity, -1, #] & @ OptionValue["MaxIterations"]
+      initialPoints = Cases[
+        OptionValue["InitialPoints"] // N,
+        List@Repeated[_Real, {dimension}],
+        {0, 1}, dimension + 1
+       ]
      },
-     compiledMinimizer = NelderMeadMinimize`Dump`CompiledNelderMead[
-       objectiveFunction, vars,
-       "HistoryLength" -> OptionValue["HistoryLength"],
-       "ReflectRatio" -> OptionValue["ReflectRatio"], "ExpandRatio" -> OptionValue["ExpandRatio"],
-       "ContractRatio" -> OptionValue["ContractRatio"], "ShrinkRatio" -> OptionValue["ShrinkRatio"],
-       CompilationTarget -> OptionValue[CompilationTarget], "ReturnValues" -> "AugmentedOptimizedParameters"
-      ];
-     initialPoints = Cases[OptionValue["InitialPoints"] // N, {Repeated[_Real, {dimensions}]}];
-     Block[{results},
-      results = compiledMinimizer[
-       initialPoints ~Join~ RandomReal[{0, 1}, {dimensions - Length[initialPoints] + 1, dimensions}],
-       OptionValue["ConvergenceTolerance"], maxIterations
-      ];
-      {First[results], Thread[vars -> Rest[results]]}
+     With[{
+       result = compiledMinimizer[
+         initialPoints~Join~RandomReal[{0, 1}, {dimension + 1 - Length[initialPoints], dimension}],
+         OptionValue["ConvergenceTolerance"], If[# === Infinity, -1, #] & @ OptionValue["MaxIterations"]
+        ]
+      },
+      {First[result], Thread[vars -> Rest[result]]}
      ]
     ]
    ];
@@ -69,8 +76,8 @@ NelderMeadMinimize[
 (* Produces inlinable code for use inside Compile (where Apply is not supported directly) *)
 ClearAll[apply];
 SetAttributes[apply, HoldRest];
-apply[f : _Function | _CompiledFunction, vars : {__Symbol}] :=
-  Function[arglist, Block[vars, vars = arglist; f @@ vars]];
+apply[f : _Function | _CompiledFunction, vars : {___Symbol}] :=
+  Function[args, Block[vars, vars = args; f @@ vars]];
 
 (* Version-specific compilable Ordering *)
 ClearAll[ordering];
@@ -80,10 +87,10 @@ ordering = If[$VersionNumber >= 8, Ordering,
   ];
 
 (* Inlinable utility function *)
-ClearAll[cumulativeAbsoluteDifferences];
-cumulativeAbsoluteDifferences = If[$VersionNumber >= 8,
-   Function[{lst}, Total@Abs@Differences[lst]/Length[lst]],
-   Function[{lst}, Total@Abs[Most[lst] - Rest[lst]]/Length[lst]]
+ClearAll[meanAbsoluteDifferences];
+meanAbsoluteDifferences = If[$VersionNumber >= 8,
+   Function[{lst}, Mean@Abs@Differences[lst]],
+   Function[{lst}, Plus @@ Abs[Most[lst] - Rest[lst]]/(Length[lst] - 1)]
   ];
 
 (* Produces compiled code for the Nelder-Mead algorithm with the objective function inlined *)
@@ -107,21 +114,22 @@ NelderMeadMinimize`Dump`CompiledNelderMead[
      (* Inlined functions *)
      f = apply[objectiveFunction, vars],
      ordering = ordering,
-     totalAbsDiffs = cumulativeAbsoluteDifferences,
+     meanAbsoluteDifferences = meanAbsoluteDifferences,
      (* Return values *)
      return = Switch[OptionValue["ReturnValues"],
        "OptimizedParameters", Function[Null, simplex[[best]]],
-       "AugmentedOptimizedParameters", Function[Null, simplex[[best]] ~Prepend~ vals[[best]]],
+       "AugmentedOptimizedParameters", Function[Null, simplex[[best]]~Prepend~values[[best]]],
        "Simplex", Function[Null, simplex],
-       "AugmentedSimplex", Function[Null, Transpose[Transpose[simplex] ~Prepend~ vals]],
-       All, Function[Null,
-        results = {vals, simplex};(* This requires a call out of the VM *)
-        {evaluations, reflections, expansions, contractions, shrinkages}
-       ],
+       "AugmentedSimplex", Function[Null, Transpose[Transpose[simplex]~Prepend~values]],
        _, Function[Null]
       ],
-     (* Options to be passed to Compile *)
-     compileopts = Sequence @@ If[$VersionNumber >= 8, {
+     (* Code transformations to be applied before compilation *)
+     transformations = If[TrueQ@OptionValue["Diagnostics"],
+       HoldPattern[h_[pre___, diagnostic[diags___], post___]] :> h[pre, diags, post],
+       HoldPattern[h_[pre___, __diagnostic, post___]] :> h[pre, post]
+      ],
+     (* Options passed to Compile *)
+     compileOpts = Sequence @@ If[$VersionNumber >= 8, {
          (* Mathematica 8 and above offer improved behaviour using these options *)
          CompilationOptions -> {"ExpressionOptimization" -> True, "InlineCompiledFunctions" -> True},
          RuntimeOptions -> {"CompareWithTolerance" -> False, "EvaluateSymbolically" -> False},
@@ -132,88 +140,103 @@ NelderMeadMinimize`Dump`CompiledNelderMead[
         }
        ]
     },
-    Compile[{{pts, _Real, 2}, {tol, _Real, 0}, {maxit, _Integer, 0}},
-     Block[{
-       (* Housekeeping *)
-       history = Table[infinity, {historyLength}], iteration = maxit,
-       (* Basic quantities *)
-       simplex = pts, vals = f /@ pts, order,
-       (* Calculated points and function values *)
-       centroid = origin,
-       reflectedPoint = origin, reflectedValue = infinity,
-       expandedPoint = origin, expandedValue = infinity,
-       contractedPoint = origin, contractedValue = infinity,
-       (* More readable indices into the simplex array *)
-       best = 1, worst = -1, rest = Range[2, Length[pts]],
-       (* Operation counts (for debugging purposes) *)
-       evaluations = Length[pts],
-       reflections = 0, expansions = 0, contractions = 0, shrinkages = 0
-      },
-      While[
-       (* Order simplex points by function value *)
-       order = ordering[vals];
-       vals = vals[[order]]; simplex = simplex[[order]];
-       (* Decrement and test iterator *)
-       (iteration--) != 0,
-       (* Check for convergence *)
-       history[[1]] = vals[[best]]; history = RotateLeft[history];
-       If[totalAbsDiffs[history] <= tol + epsilon totalAbsDiffs[history],
-        Break[]
-       ];
-       (* Find centroid of best (N - 1) points *)
-       centroid = Mean@Most[simplex];
-       (* Reflect *)
-       reflectedPoint = centroid + reflectRatio (centroid - simplex[[worst]]);
-       reflectedValue = f[reflectedPoint]; ++evaluations;
-       If[vals[[best]] <= reflectedValue < vals[[-2]],
-        vals[[worst]] = reflectedValue; simplex[[worst]] = reflectedPoint;
-        ++reflections; Continue[]
-       ];
-       (* Expand *)
-       If[reflectedValue < vals[[best]],
-        expandedPoint = centroid + expandRatio (reflectedPoint - centroid);
-        expandedValue = f[expandedPoint]; ++evaluations;
-        If[expandedValue < reflectedValue,
-         vals[[worst]] = expandedValue; simplex[[worst]] = expandedPoint;
-         ++expansions; Continue[],
-         vals[[worst]] = reflectedValue; simplex[[worst]] = reflectedPoint;
-         ++reflections; Continue[]
-        ];
-       ];
-       (* Contract *)
-       If[reflectedValue < vals[[worst]],
-        (* Outside contraction *)
-        contractedPoint = centroid + contractRatio (reflectedPoint - centroid);
-        contractedValue = f[contractedPoint]; ++evaluations;
-        If[contractedValue <= reflectedValue,
-         vals[[worst]] = contractedValue; simplex[[worst]] = contractedPoint;
-         ++contractions; Continue[]
-        ];,
-        (* Inside contraction *)
-        contractedPoint = centroid - contractRatio (centroid - simplex[[worst]]);
-        contractedValue = f[contractedPoint]; ++evaluations;
-        If[contractedValue < vals[[worst]],
-         vals[[worst]] = contractedValue; simplex[[worst]] = contractedPoint;
-         ++contractions; Continue[]
-        ];
-       ];
-       (* Shrink *)
-       simplex[[rest]] = simplex[[best]] + shrinkRatio (simplex[[rest]] - simplex[[best]]);
-       vals[[rest]] = f /@ simplex[[rest]]; evaluations += Length[rest] - 1;
-       ++shrinkages;
-      ];
-      return[]
-     ], compileopts
-    ]
+    Compile @@ (
+      Hold[{{initialSimplex, _Real, 2}, {tolerance, _Real, 0}, {maxIterations, _Integer, 0}},
+        Block[{
+          (* Housekeeping *)
+          history = Table[infinity, {historyLength}], iteration = maxIterations,
+          (* Basic quantities *)
+          simplex = initialSimplex, values = f /@ initialSimplex, order,
+          (* Calculated points and function values *)
+          centroid = origin,
+          reflectedPoint = origin, reflectedValue = infinity,
+          expandedPoint = origin, expandedValue = infinity,
+          contractedPoint = origin, contractedValue = infinity,
+          (* More readable indices into the simplex array *)
+          best = 1, worst = -1, rest = Range[2, Length[initialSimplex]],
+          diagnostic[
+           (* Operation counts *)
+           evaluations = Length[initialSimplex],
+           reflections = 0, expansions = 0, contractions = 0, shrinkages = 0
+          ]
+         },
+         While[
+          (* Order simplex points by function value *)
+          order = ordering[values];
+          values = values[[order]]; simplex = simplex[[order]];
+          (* Decrement and test iterator *)
+          (iteration--) != 0,
+          (* Check for convergence *)
+          history[[1]] = values[[best]]; history = RotateLeft[history];
+          If[meanAbsoluteDifferences[history] <= tolerance + epsilon meanAbsoluteDifferences[history],
+           Break[]
+          ];
+          (* Find centroid of best (N - 1) points *)
+          centroid = Mean@Most[simplex];
+          (* Reflect *)
+          reflectedPoint = centroid + reflectRatio (centroid - simplex[[worst]]);
+          reflectedValue = f[reflectedPoint]; diagnostic[++evaluations];
+          If[values[[best]] <= reflectedValue < values[[-2]],
+           values[[worst]] = reflectedValue; simplex[[worst]] = reflectedPoint;
+           diagnostic[++reflections]; Continue[]
+          ];
+          (* Expand *)
+          If[reflectedValue < values[[best]],
+           expandedPoint = centroid + expandRatio (reflectedPoint - centroid);
+           expandedValue = f[expandedPoint]; diagnostic[++evaluations];
+           If[expandedValue < reflectedValue,
+            values[[worst]] = expandedValue; simplex[[worst]] = expandedPoint;
+            diagnostic[++expansions]; Continue[],
+            values[[worst]] = reflectedValue; simplex[[worst]] = reflectedPoint;
+            diagnostic[++reflections]; Continue[]
+           ]
+          ];
+          (* Contract *)
+          If[reflectedValue < values[[worst]],
+           (* Outside contraction *)
+           contractedPoint = centroid + contractRatio (reflectedPoint - centroid);
+           contractedValue = f[contractedPoint]; diagnostic[++evaluations];
+           If[contractedValue <= reflectedValue,
+            values[[worst]] = contractedValue; simplex[[worst]] = contractedPoint;
+            diagnostic[++contractions]; Continue[]
+           ],
+           (* Inside contraction *)
+           contractedPoint = centroid - contractRatio (centroid - simplex[[worst]]);
+           contractedValue = f[contractedPoint]; diagnostic[++evaluations];
+           If[contractedValue < values[[worst]],
+            values[[worst]] = contractedValue; simplex[[worst]] = contractedPoint;
+            diagnostic[++contractions]; Continue[]
+           ]
+          ];
+          (* Shrink *)
+          simplex[[rest]] = simplex[[best]] + shrinkRatio (simplex[[rest]] - simplex[[best]]);
+          values[[rest]] = f /@ simplex[[rest]]; diagnostic[evaluations += Length[rest]];
+          diagnostic[++shrinkages];
+         ];
+         diagnostic[
+          NelderMeadMinimize`Dump`OperationCounts = {
+            "Function evaluations" -> evaluations,
+            "Reflections" -> reflections, "Expansions" -> expansions,
+            "Contractions" -> contractions, "Shrinkages" -> shrinkages
+           }
+         ];
+         return[]
+        ], compileOpts
+       ] //. transformations
+     )
    ];
 
-(* Unimodal convex function; minimum at {1, 1, ...} *)
-NelderMeadMinimize`Dump`Hyperellipsoid = 
-  Block[{x = {##}}, Plus @@ (Range@Length[x] (x - 1)^2)] &;
+(* Unimodal convex separable function; minimum at {1, 1, ...} *)
+NelderMeadMinimize`Dump`Hyperellipsoid =
+  With[{x = {##}}, Plus @@ (Range@Length[x] (x - 1)^2)] &;
+
+(* Unimodal convex nonseparable function; minimum at {1, 1, ...} *)
+NelderMeadMinimize`Dump`RotatedHyperellipsoid =
+  With[{x = {##}}, Plus @@ (FoldList[Plus, 0, x - 1]^2)] &;
 
 (* Non-convex function; global minimum at {1, 1, ...} with secondary local minimum near {-1, 1, 1, ...} *)
-NelderMeadMinimize`Dump`Rosenbrock = 
-  Block[{x = Most[{##}], y = Rest[{##}]}, Plus @@ (100 (y - x^2)^2 + (1 - x)^2)] &;
+NelderMeadMinimize`Dump`Rosenbrock =
+  With[{x = Most[{##}], y = Rest[{##}]}, Plus @@ (100 (y - x^2)^2 + (1 - x)^2)] &;
 
 End[];
 
